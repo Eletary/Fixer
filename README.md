45b8f88192b0632825c66891622d844557732d56
ZEROCode <zeroetime@outlook.com>
2021-03-28 16:59:05 +0800

    init as 4.4

---
+++ README.md
@@ -0,0 +1,18 @@
+# Virus Fixer v4 使用说明
+[Virus Fixer](https://files.cnblogs.com/files/zerocode/NFLSVirusFixer.zip) 由 [NFLSCode](https://cnblogs.com/zerocode) 开发。
+
+该程序的目的是为了帮助您消灭 [Babonock.A](https://www.microsoft.com/en-us/wdsi/threats/malware-encyclopedia-description?name=win32/babonock)。
+
+该程序仅能在*Microsoft Windows*上运行。
+
+具体方法：
+
++ 如果您的电脑感染了病毒，将 Fixer.exe（压缩包附带）放置在任意一个磁盘中（E.X. C:）即可消灭病毒。
+
++ 如果您的U盘（闪存盘）感染了病毒，将 Fixer.exe 放置在U盘（闪存盘）根目录下即可。
+
+---
+
+如有疑问或想要源代码，请发邮件至 zeroetime@126.com。
+
+感谢您对本项目的支持。
---
+++ Recover.cpp
@@ -0,0 +1,84 @@
+//1 - show str(file/folder) & delete *.exe
+//2 - show str(file/folder) & delete str.exe & hide System Volume Information (folder)
+//3 - ditto but str is a folder
+//4 - ditto and clean virus in hard drives (this file)
+//5 - ditto and clean the regedit
+#include <bits/stdc++.h>
+#include <dirent.h>
+#include <windows.h>
+using namespace std;
+BOOL FindFirstFileExists(LPCTSTR lpPath, DWORD dwFilter)
+{
+    WIN32_FIND_DATA fd;
+    HANDLE hFind = FindFirstFile(lpPath, &fd);
+    BOOL bFilter = (FALSE == dwFilter) ? TRUE : fd.dwFileAttributes & dwFilter;
+    BOOL RetValue = ((hFind != INVALID_HANDLE_VALUE) && bFilter) ? TRUE : FALSE;
+    FindClose(hFind);
+    return RetValue;
+}
+BOOL FilePathExists(LPCTSTR lpPath)
+{
+    return FindFirstFileExists(lpPath, FALSE);
+}
+bool IsFolder(const char* tempPath)
+{
+	WIN32_FIND_DATAA FindFileData;
+    FindFirstFileA((LPCSTR)tempPath,&FindFileData);
+    if(FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        return true;
+    else
+        return false;
+}
+void computer();
+void udisk();
+int main()
+{
+	cout << "------------------------------------------------\n"
+			"             Babonock.A Fixer v4.4\n" 
+			"           Copyright 2020 by NFLSCode.\n"
+			"              All rights reserved.\n"
+			"------------------------------------------------"<<endl;
+    freopen("Fixer.log","w",stdout);
+    computer();
+    udisk();
+    return 0;
+}
+void computer()
+{
+    if (FilePathExists("%APPDATA%\\Microsoft\\Office\\rundll.exe"))
+    {
+        cout<<"Virus found in the drive.\n";
+        system("chdir /D %APPDATA%\\Microsoft\\Office"
+        	   "&del mspoint.pip"
+        	   "&attrib -h -s rundll32.exe"
+        	   "&taskkill /f /im rundll32.exe /t" 
+               "&del rundll32.exe");
+        cout<<"Virus fixed.\n";
+    }
+    else cout<<"Virus not found in the drive.\n";
+}
+void udisk()
+{
+    if (!FilePathExists("System Volume Information.exe"))
+    {
+       cout<<"Not in a removable dirve (root) or no virus found.\n";
+       return;
+    }
+    cout<<"Fixing the removable drive...\n";
+    DIR *dir;
+    dirent *ptr;
+    dir = opendir(".");
+    while ((ptr = readdir(dir)) != NULL)
+    {
+        string ss = ptr->d_name;
+        if (IsFolder(ss.c_str()))
+        {
+        	if (ss=="."/*||ss=="System Volume Information.exe"*/) continue;
+            system(("attrib /s /d -h -s \"" + ss + "\"").c_str());
+           	system(("del \"" + ss + ".exe\"").c_str());
+        }
+    }
+    closedir(dir);
+    system("attrib /s /d +h +s \"System Volume Information\"");
+    cout<<"The removable drive is fixed.";
+}
